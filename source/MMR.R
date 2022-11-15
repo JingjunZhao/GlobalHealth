fa37d24405ad7b5d56b3fa868e974c0dea4ed273
Becky Zhao <xiaobezhao@MacBook-Air-72.localdomain>
2022-11-14 22:45:30 -0800

    <+MMR>

--- source/MMR.R
+++ source/MMR.R
@@ -1,7 +1,7 @@
 ## maternal mortality ratio
 
-mmr_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-af/main/data/API_SH.STA.MMRT_DS2_en_csv_v2_4693917.csv")
-View(mmr_data)
+maternal_mortality_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-af/main/data/API_SH.STA.MMRT_DS2_en_csv_v2_4693917.csv")
+
 
 install.packages("ggplot2")
 install.packages("dplyr")
@@ -14,18 +14,15 @@ library(viridis)
 
 
 # organizing data
-mmr_data <- subset(mmr_data, select = -c(X2000,X2001,X2002,X2003,
-                                         X2004,X2005,X2006,X2007,X2008,X2009,
-                                         X2010,X2011,X2012,X2013,X2014,X2015,X2016))
-mmr_data <- na.omit(mmr_data)
+mmr_data<-group_by(maternal_mortality_data,Country.Name)%>%summarise(X2017=X2017,Country.Name=Country.Name)
+names(mmr_data)[1]<-paste("Country")
+names(mmr_data)[2]<-paste("maternal_mortality")
 
 # Plot
-as.numeric(mmr_data$X2017)
-barplot(mmr_data$X2017)
-mmr_plot <- barplot(mmr_data$X2017, names.arg = mmr_data$Country.Name, main = "Maternal Mortality Rate Across Countries in 2017", 
-                    xlab="Country Name", ylab = "Maternal Mortality")
 
-
+barplot(mmr_data$maternal_mortality)
+mmr_plot <- barplot(mmr_data$maternal_mortality, names.arg = mmr_data$Country,main = "Maternal Mortality Rate Across Countries in 2017", 
+                    xlab="Country", ylab = "maternal_mortality")
 plot(mmr_plot)
 
 
