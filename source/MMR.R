896e1619b80fea7533746b59a9ac92c0c6375c85
Abigail Babila <abigailbabila@Abigails-MacBook-Pro.local>
2022-12-04 10:27:10 -0800

    chart 3

--- source/MMR.R
+++ source/MMR.R
@@ -1,26 +1,44 @@
 ## maternal mortality ratio
 
 maternal_mortality_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-af/main/data/API_SH.STA.MMRT_DS2_en_csv_v2_4693917.csv")
+View(maternal_mortality_data)
 
-
-install.packages("ggplot2")
-install.packages("dplyr")
-install.packages("hrbrthemes")
-install.packages("virdis")
 library(ggplot2)
 library(dplyr)
 library(hrbrthemes)
 library(viridis)
-
+library(plotly)
 
 # organizing data
-mmr_data<-group_by(maternal_mortality_data,Country.Name)%>%summarise(X2017=X2017,Country.Name=Country.Name)
+mmr_data <- na.omit(mmr_data)
+mmr_data<-mmr_data[-c(83, 86, 88, 89, 90, 91, 115, 121, 122, 123, 137, 138, 155, 157, 167, 168, 189, 197, 218, 225),]
+mmr_data <- group_by(maternal_mortality_data,Country.Name)%>%summarise(X2017=X2017,Country.Name=Country.Name)
 names(mmr_data)[1]<-paste("Country")
 names(mmr_data)[2]<-paste("maternal_mortality")
 
-# Plot
+View(mmr_data)
+# plot
 mmr_plot <- barplot(mmr_data$maternal_mortality, names.arg = mmr_data$Country,main = "Maternal Mortality Rate Across Countries in 2017", 
                     xlab="Country", ylab = "maternal_mortality")
 plot(mmr_plot)
+
+ggplotly(mmr_plot)
+
+## ggplot plot
+
+mortality_data <- mmr_data %>%
+  group_by(Country,maternal_mortality) %>%
+  summarise(n=n(), maternal_mortality = sum(maternal_mortality))
+
+plot_mmr <- function () {
+  summary <- ggplot(mortality_data, aes(y = maternal_mortality, x=Country)) + 
+    ggtitle("Maternal Morality Rate Across Countries in 2017") +
+    geom_bar(stat="identity", color="turquoise")
+  return(summary)
+}
+
+plot_mmr()
+
+plot_mmr <- ggplotly(plot_mmr)
 
 
