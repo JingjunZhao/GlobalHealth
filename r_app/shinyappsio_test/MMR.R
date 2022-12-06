5ce925a66942a9ab830b58e07951b1674a6b732b
emma-esteboss <esteb@uw.edu>
2022-12-06 10:04:33 -0800

    add shinyapp test

---
+++ r_app/shinyappsio_test/MMR.R
@@ -0,0 +1,37 @@
+## maternal mortality ratio
+
+maternal_mortality_data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-1-section-af/main/data/API_SH.STA.MMRT_DS2_en_csv_v2_4693917.csv")
+
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+library(viridis)
+library(plotly)
+
+# organizing data
+mmr_data <- group_by(maternal_mortality_data,Country.Name)%>%summarise(X2017=X2017,Country.Name=Country.Name)
+mmr_data <- na.omit(mmr_data)
+mmr_data<-mmr_data[-c(83, 86, 88, 89, 90, 91, 115, 121, 122, 123, 137, 138, 155, 157, 167, 168, 189, 197, 218, 225),]
+names(mmr_data)[1]<-paste("Country")
+names(mmr_data)[2]<-paste("maternal_mortality")
+
+
+## ggplot plot
+
+mortality_data <- mmr_data %>%
+  group_by(Country,maternal_mortality) %>%
+  summarise(n=n(), maternal_mortality = sum(maternal_mortality))
+
+plot_mmr <- function () {
+  summary <- ggplot(mortality_data, aes(y = maternal_mortality, x=Country)) + 
+    ggtitle("Maternal Morality Rate Across Countries in 2017") +
+    geom_bar(position = "dodge", stat="identity", color="gray")
+  return(summary)
+}
+
+plot_mmr()
+
+plot_mmr <- ggplotly(plot_mmr())
+plot_mmr
+
+
---
+++ r_app/shinyappsio_test/chart_2.R
@@ -0,0 +1,48 @@
+# Chart 2 - Scatter Plot - Debt and Health Spending
+library("dplyr")
+library("tidyverse")
+library("ggplot2")
+library("plotly")
+
+# Import Data
+debt_data <- read.csv("../shinyappsio_test/debt_data_portal.csv", stringsAsFactors = FALSE)
+health_spending_data <- read.csv("../shinyappsio_test/expenditure_as_a_share.csv", stringsAsFactors = FALSE)
+
+## Editing debt data
+debt <- debt_data %>%
+  rename("JDC_risk_2021" = JDC.Risk..2021.) %>%
+  rename("net_creditor_debtor_2020" = Net.creditor...debtor..2020.)
+
+## Editing health spending data
+health_spending <- health_spending_data %>%
+  rename("health_perc_expenditure" = Indicator.Domestic.general.government.health.expenditure..GGHE.D..as.percentage.of.general.government.expenditure..GGE.....) %>%
+  rename("Country" = Entity) %>%
+  group_by(Country) %>%
+  summarize(Year = Year,
+            most_recent_year = max(Year),
+            health_perc_expenditure = health_perc_expenditure) %>%
+  filter(Year == most_recent_year) %>%
+  subset(select = -(most_recent_year))
+
+## Join debt and health spending tables
+debt_and_health_spending <- health_spending %>%
+  left_join(debt, by = "Country")
+
+## Plotting
+plot <- ggplot(data = debt_and_health_spending) +
+  geom_point(mapping = aes(x = net_creditor_debtor_2020,
+                           y = health_perc_expenditure,
+                           color = JDC_risk_2021)) +
+  geom_smooth(mapping = aes(x = net_creditor_debtor_2020,
+                            y = health_perc_expenditure),
+              method = loess,
+              formula = y ~ x) +
+  labs(title = "Net Debt vs. Health Spending",
+       subtitle = "Subtitle",
+       caption = "This is what the data shows.",
+       x = "Net Creditor/Debtor",
+       y = "Health Spending (% of Govt. Expenditure)",
+       color = "JDC Risk (2021)")
+chart_2 <- ggplotly(plot)
+print(chart_2)
+
